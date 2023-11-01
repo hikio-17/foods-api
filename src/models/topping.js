163c88b3e706a96f92d171a64e5623b14ad35a46
hikio-17 <fajri010217@gmail.com>
2023-11-01 08:52:07 +0700

    create topping model

---
+++ src/models/topping.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema;
+
+const toppingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Name is required',
+  },
+  price: {
+    type: Number,
+    required: 'Price is requried',
+  },
+  parent: [
+    {
+      type: ObjectId,
+      required: true,
+    },
+  ],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Topping', toppingSchema);
