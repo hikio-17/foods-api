7f43c0895e49fe0283a49f429b3109d957f350de
hikio-17 <fajri010217@gmail.com>
2023-11-01 11:31:31 +0700

    create router for food

--- src/routes/foodRouter.js
+++ src/routes/foodRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const { addFoodHandler } = require('../controllers/foodController');
+const {
+  addFoodHandler,
+  getAllFoodHandler,
+  getFoodByIdHandler,
+  updateFoodByIdHandler,
+  deleteFoodByIdHandler,
+} = require('../controllers/foodController');
 
 const router = express.Router();
 
+router.get('/foods', getAllFoodHandler);
+router.get('/foods/:id', getFoodByIdHandler);
 router.post('/foods', addFoodHandler);
+router.put('/foods/:id', updateFoodByIdHandler);
+router.delete('/foods/:id', deleteFoodByIdHandler);
 
 module.exports = router;
